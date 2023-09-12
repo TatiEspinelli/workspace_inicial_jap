47c98f4649fdd5cfbc93e0bbe281495d98d7af70
Tati Espinelli <tatiespinelli@gmail.com>
2023-09-12 12:39:39 -0300

    desafiate

--- js/product-info.js
+++ js/product-info.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded", async () => {
+  let username = localStorage.getItem("username"); // Obtener el nombre de usuario
+
+  comments = []; // Esta variable almacenará los comentarios
+
   // Obtenemos el identificador del producto guardado en el almacenamiento local
   const selectedProductId = localStorage.getItem("selectedProductId");
 
@@ -106,10 +110,10 @@ document.addEventListener("DOMContentLoaded", async () => {
       return starContainer;
     }
 
-    // Obtenemos y mostrar los comentarios del producto
+    // Obtenemos y mostramos los comentarios del producto
     const productCommentsUrl = `${PRODUCT_INFO_COMMENTS_URL}${selectedProductId}${EXT_TYPE}`;
     const commentsResponse = await fetch(productCommentsUrl);
-    const comments = await commentsResponse.json();
+    let comments = await commentsResponse.json();
 
     // Creamos una sección para mostrar los comentarios
     const commentsSection = document.createElement("div");
@@ -190,14 +194,82 @@ document.addEventListener("DOMContentLoaded", async () => {
       const commentText = document.getElementById("comment-text").value;
       const commentRating = document.getElementById("comment-rating").value;
 
-      // Aquí puedes realizar acciones con el comentario y la calificación,
-      // como mostrarlos en la página o enviarlos al servidor (no implementado en este ejemplo)
 
-      
+      // Validamos que se haya ingresado un comentario
+      if (!commentText) {
+        alert("Por favor, ingrese un comentario.");
+        return;
+      }
 
-      // Limpiar el formulario o realizar otras acciones según sea necesario
+      // Creamos un objeto para representar el comentario
+      const newComment = {
+        comment: commentText,
+        score: parseInt(commentRating),
+        user: username, 
+        dateTime: new Date().toLocaleString(), 
+      };
+
+      // Agregamos el nuevo comentario a la lista existente
+      comments.push(newComment);
+
+      // Almacenamos la lista actualizada en el almacenamiento local
+      localStorage.setItem("comments", JSON.stringify(comments));
+
+      // Limpiamos el formulario 
       commentForm.reset();
+
+      // Actualizamos la vista de los comentarios
+      updateCommentsView();
     });
+
+    // Función para actualizar la vista de los comentarios
+    function updateCommentsView() {
+      // Eliminamos todos los comentarios actuales en la vista
+      const commentsSection = document.querySelector(".mt-4");
+      commentsSection.innerHTML = "";
+
+      // Recorremos la lista de comentarios y los mostramos en la vista
+      comments.forEach((comment) => {
+        // Creamos elementos de la vista para cada comentario
+        const commentCard = document.createElement("div");
+        commentCard.classList.add("card", "mb-3", "w-50", "mx-auto");
+
+        const commentCardBody = document.createElement("div");
+        commentCardBody.classList.add("card-body");
+
+        const commentText = document.createElement("p");
+        commentText.textContent = comment.comment;
+
+        const commentUser = document.createElement("p");
+        commentUser.textContent = comment.user;
+        commentUser.classList.add(
+          "text-start",
+          "fw-bold",
+          "d-inline",
+          "m-4",
+          "responsiveComment"
+        );
+
+        const commentDate = document.createElement("p");
+        commentDate.textContent = comment.dateTime;
+        commentDate.classList.add(
+          "text-end",
+          "d-inline",
+          "m-4",
+          "responsiveComment"
+        );
+
+        const commentRating = createStarRating(comment.score); // Llama a la función para crear las estrellas
+
+        commentCardBody.appendChild(commentUser);
+        commentCardBody.appendChild(commentDate);
+        commentCardBody.appendChild(commentText);
+        commentCardBody.appendChild(commentRating);
+
+        commentCard.appendChild(commentCardBody);
+        commentsSection.appendChild(commentCard);
+      });
+    }
 
     // Textarea para el comentario
     const commentTextarea = document.createElement("textarea");
@@ -255,6 +327,12 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     // Agregar sección de comentarios al contenedor de información del producto
     productInfoContainer.appendChild(commentFormContainer);
+
+    // Cargar comentarios almacenados en el almacenamiento local 
+    if (localStorage.getItem("comments")) {
+      comments = JSON.parse(localStorage.getItem("comments"));
+      updateCommentsView(); // Actualizar la vista de los comentarios
+    }
   } catch (error) {
     console.error(
       "Error al obtener y mostrar la información del producto:",
